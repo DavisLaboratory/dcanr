4276d509cdedce4564b944aeafc2868e790dbb72
bhuvad <bhuva.d@wehi.edu.au>
2018-09-26 10:32:05 +1000

    Added test cases for dcEvaluate

--- tests/testthat/test_evaluation.R
+++ tests/testthat/test_evaluation.R
@@ -1,6 +1,7 @@
 library(dcevalr)
 library(igraph)
 library(stringr)
+library(EBcoexpress)
 
 context('Simulation accessors ')
 
@@ -12,6 +13,17 @@ test_that('Result result of pipeline ', {
     expect_equal(length(res), 2, info = m)
     expect_equal(as.numeric(lapply(res, function(x) length(V(x)))), rep(nrow(sim102$data), 2), info = m)
     expect_equal(as.character(lapply(res, class)), rep('igraph', 2), info = m)
+  }
+})
+
+test_that('Result result of pipeline ', {
+  for(m in c('zscore', 'diffcoex', 'dicer', 'ebcoexpress')) {
+    nets = dcPipeline(sim102, dc.func = m)
+    res = dcEvaluate(sim102, dclist = nets, perf.method = 'f.measure', combine = TRUE)
+    expect_equal(length(res), 1, info = m)
+    res = dcEvaluate(sim102, dclist = nets, perf.method = 'f.measure', combine = FALSE)
+    expect_equal(length(res), length(nets), info = m)
+    expect_equal(names(res), names(nets), info = m)
   }
 })
 
--- tests/testthat/test_simulation_accessors.R
+++ tests/testthat/test_simulation_accessors.R
@@ -10,7 +10,7 @@ test_that('Network retreival works ', {
   for (type in c('direct', 'influence', 'association')) {
     #networks retrieved
     nets = lapply(getConditionNames(sim102), function(x)
-      getTrueNetwork(sim102, x, what = type))
+      getTrueNetwork(sim102, x, truth.type = type))
     nets = lapply(nets, graph_from_adjacency_matrix, mode = 'undirected')
     netlens = lapply(nets, function(x)
       length(E(x)))
@@ -26,9 +26,9 @@ test_that('Edges are the same when using the full dataset ', {
   for (type in c('direct', 'influence', 'association')) {
     #networks retrieved
     nets = lapply(getConditionNames(sim102), function(x)
-      sum(getTrueNetwork(sim102, x, what = type)))
+      sum(getTrueNetwork(sim102, x, truth.type = type)))
     nets_full = lapply(getConditionNames(sim102), function(x)
-      sum(getTrueNetwork(sim102, x, TRUE, what = type)))
+      sum(getTrueNetwork(sim102, x, TRUE, truth.type = type)))
 
     expect_equal(sum(unlist(nets)), sum(unlist(nets_full)))
   }
@@ -49,7 +49,7 @@ test_that('Retrieved network vector is same as stored network ', {
   for (type in names(vecnames)) {
     #networks retrieved
     nets = lapply(getConditionNames(sim102), function(x){
-      mat = getTrueNetwork(sim102, x, what = type)
+      mat = getTrueNetwork(sim102, x, truth.type = type)
       mat = mat[sort(rownames(mat)), sort(rownames(mat))]
       return(mat2vec(mat))
     })
